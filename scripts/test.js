599229e3c78f4177e5fecafac61445a57e3c0880
Chris Jackson <chrisjackson0416@gmail.com>
2022-03-19 20:27:30 -0700

    adding import and refactoring typing text

--- scripts/test.js
+++ scripts/test.js
@@ -1,14 +1,19 @@
+import {startTime} from './timer.js';
+
 function startTest() {
     fillText();
+    startTime();
 }
 
 function fillText() {
-    document.getElementById("typing-content").innerHTML = "Lorem ipsum" 
-    + "dolor sit amet, consectetur adipiscing elit, sed do eiusmod" 
-    + "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" 
-    + "minim veniam, quis nostrud exercitation ullamco laboris nisi ut" 
-    + "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" 
-    + "in voluptate velit esse cillum dolore eu fugiat nulla pariatur." 
-    + "Excepteur sint occaecat cupidatat non proident, sunt in culpa" 
-    + "qui officia deserunt mollit anim id est laborum."
+    document.getElementById("typing-content").innerHTML = `Lorem ipsum 
+    dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
+    minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+    qui officia deserunt mollit anim id est laborum.`
 }
+
+startTest();
