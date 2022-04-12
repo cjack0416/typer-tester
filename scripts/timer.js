cb90656dc4065bed4e736fb173367f08e29169ce
Chris Jackson <chrisjackson0416@gmail.com>
2022-04-11 20:48:17 -0700

    Moving calculating and displaying wpm in timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -9,16 +9,19 @@ function startTime() {
     const interval = setInterval(function() {
         document.getElementById("timer").innerHTML = `time left: ${timer}`;
         timer--;
-
-        timePerMin = (60 - timer) / 60;
-
-        wpm = Math.round((currChar / 5) / timePerMin);
-
-        document.getElementById("wpm").innerHTML = `wpm: ${wpm}`;
     
         if (timer < 0) clearInterval(interval);
     }, 1000);
+    
+    const wpmInterval = setInterval(function() {
+        timePerMin = (60 - timer) / 60;
 
+        wpm = timePerMin !== 0 ? Math.round((currChar / 5) / timePerMin) : 0;
+
+        document.getElementById("wpm").innerHTML = `wpm: ${wpm}`;
+
+        if (timer < 0) clearInterval(wpmInterval);
+    }, 1);
 }
 
 export {startTime, timePerMin, timer};
